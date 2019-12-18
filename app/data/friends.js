4f9252ea6c426e6377b3199e8c418106db109787
Arek Chatmajian <arek.chatmajian@gmail.com>
2019-12-18 15:33:14 -0500

    added htmlroutes and public layouts

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+	{
+        // George Washington
+		name: "George",
+		photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2NDcwODAyODk0NDk2OTI5/gilbert_stuart_williamstown_portrait_of_george_washington_promo.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+        // Abraham Lincoln
+		name: "Abraham",
+		photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU5MDUzMTE0Mzk2MTI0OTUy/abraham-lincoln-1809---18652c-sixteenth-president-of-the-united-states-of-america-photo-by-stock-montagestock-montagegetty-images_promo.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+        // Thomas Jefferson
+		name: "Thomas",
+		photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2NzAyNTE0NTAxMDY3ODMx/thomas-jefferson_editedjpg.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+        // Benjamin Franklin
+		name: "Benjamin",
+		photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2NTIxNzUwNjAxODY4NTEx/benjamin-franklin_editedjpg.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+        // Andrew Jackson
+		name: "Andrew",
+		photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1MDA4MTUxMDU1/andrew-jackson-9350991-1-402.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -3,7 +3,16 @@ var path = require("path");
 var fs = require("fs");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+require('./app/routing/apiRoutes.js')(app, path);
+require('./app/routing/htmlRoutes.js')(app, path);
+
+
+app.listen(PORT, function() {
+    console.log("Listening on port: " + PORT);
+})
